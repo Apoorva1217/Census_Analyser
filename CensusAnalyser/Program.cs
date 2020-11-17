8b40881220c2397d1e4688bcdaca19a10c4194ca
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-17 18:50:11 +0530

    [Apoorva] Add . Welcome to Census Analyser Program

---
+++ CensusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Census Analyser Program!");
+        }
+    }
+}
