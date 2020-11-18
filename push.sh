1ba61483772cd174d3a904cdd60232ca58d1fcca
Apoorva1217 <62049073+Apoorva1217@users.noreply.github.com>
2020-11-18 16:14:50 +0530

    .

--- push.sh
+++
@@ -1,9 +0,0 @@
-read a
-git branch $a
-git checkout $a
-git add .
-git commit -m "[Apoorva] Add . Ability for the analyser to load the Indian States Code Information from a csv file"
-git push origin $a
-git checkout master
-git merge $a
-git push origin master --force
